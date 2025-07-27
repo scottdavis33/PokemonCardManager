528478822b69e3bd60f43eae75617b2a6b6f6fea
Scott Davis <scda2202@colorado.edu>
2025-07-27 16:12:48 -0600

    File structure completed

---
+++ data/init-mogno.js
@@ -0,0 +1,3 @@
+db = db.getSiblingDB('pokecarddb');
+// Use mongoimport or JS loops to load files (but since files are local, mount them and run mongoimport in a script).
+// Alternatively, handle import in Go seed command.
